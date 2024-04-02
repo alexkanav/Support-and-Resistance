7d0d8068ac622bc1258e408934c54983cdb6d963
alexkanav <72207416+alexkanav@users.noreply.github.com>
2024-04-02 16:47:20 +0300

    color change

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def draw_candle_chart(sample_df, lines, region=None):
     plt.bar(up.index, up.low-up.open, width2, bottom=up.open, color=col1)
         # plot down prices
     plt.bar(down.index, down.close - down.open, width, bottom=down.open, color=col2)
-    plt.bar(down.index, down.high - down.open, width2, bottom=down.open, color=col1)
-    plt.bar(down.index, down.low - down.close, width2, bottom=down.close, color=col1)
+    plt.bar(down.index, down.high - down.open, width2, bottom=down.open, color=col2)
+    plt.bar(down.index, down.low - down.close, width2, bottom=down.close, color=col2)
         # rotate x-axis tick labels
     plt.xticks(rotation=45, ha='right')
 
