233b317e633bf03487a77373b0d56a306ffee13c
alex <sserinua@gmail.com>
2025-04-08 15:50:26 +0300

    Add color lines.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 csv_file = "BTCUSDTh.csv"     # csv file for analysis
-
 slice_ = slice(0, 250)        # selects lines from csv file
-
 peaks_range = [2, 3]          # number of lines
+color_1 = 'green'
+color_2 = 'red'
